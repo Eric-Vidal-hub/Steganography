8c3e86f7ec86be0e6be531ce43c677d5bb42de16
Eric Vidal <ericvidalmarcos@gmail.com>
2023-12-03 17:08:36 +0100

    healthy until last example main

--- main.py
+++ main.py
@@ -10,16 +10,17 @@ or image is hidden by changing the least significant bits of the DCT
 coefficients of the cover image. The secret message or image is recovered by
 extracting the least significant bits of the DCT coefficients of the stego
 image. The secret message or image is hidden by changing the least significant
-bits of the DCT coefficients of the cover image. 
+bits of the DCT coefficients of the cover image.
 """
+from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import data
 from skimage.metrics import structural_similarity as ssim
 from skimage.transform import rescale
-from collections import Counter
-# from functions import bintostring, stringtobin, embedding, dembedding
-from functions_refact import bintostring, stringtobin, embedding, dembedding
+# from functions import embedding, dembedding
+from functions_refact import embedding, dembedding
+
 
 class Node:
     '''
@@ -126,12 +127,12 @@ def huffman_encoding(data_in):
         left.code = 0
         right.code = 1
         # Merge the two nodes with frequency summed up
-        newNode = Node(left.prob+right.prob, left.symbol +
+        newnode = Node(left.prob+right.prob, left.symbol +
                        right.symbol, left, right)
         # Remove the nodes that we have already merged
         nodes.remove(left)
         nodes.remove(right)
-        nodes.append(newNode)   # and introduce the new one
+        nodes.append(newnode)   # and introduce the new one
 
     # Start from the top and go down toe get the code of each symbol
     huffman_coding = calculate_codes(nodes[0])
@@ -176,48 +177,29 @@ def huffman_decoding(encod_data, huffmantree):
     return decoded_output
 
 
-# %% 3. EXAMPLES WITHOUT HUFFMAN TREE
-'''We'll show the implementation of DCT Steganography in two examples:
-character string and secret image.'''
-
-
-# %%% 3.1. CHARACTERS STRING
-print('EXAMPLE 3.1. CHARACTERS STRING')
-
+print('\nEXAMPLES OF DCT STEGANOGRAPHY WITH HUFFMAN ENCODING\n')
+print('1. CHARACTERS STRING')
 im = data.camera()  # Cover image
+MESSAGE = 'Steganography among other rare disciplines is honored to be ' \
+          'described as both an art and Science field.'     # Secret message
+print('Num of characters in our message:', len(MESSAGE))
+encod_text, huffmantree = huffman_encoding(MESSAGE)
+binarymessage = np.array([int(i) for i in encod_text], dtype='uint8')
 
-# Secret message, this is the typical one:
-message = 'Steganography among other rare disciplines is honored to be described\
- as both an art and Science field.'
+SQR_SIDE = 8  # Size of the blocks to be processed: Common info
+BINMESLEN = len(binarymessage)  # Common info
 
-# Firstly we wanna know how many characters has our message
-print('Num of characters in our message:', len(message))
+steg = embedding(im, binarymessage, SQR_SIDE)   # Sender: stego image
+# Receiver: secret message and recons image
+SECMES, recons = dembedding(steg, BINMESLEN, SQR_SIDE)
 
-# We apply the proper function to get its corresponding binary values
-binarymessage = stringtobin(message)
+# create a string from secmes 1D-array to decode thanks to huffman tree
+SECMES = ''.join(str(i) for i in SECMES)
+decod_text = huffman_decoding(SECMES, huffmantree)
 
-squarelength = 8  # square length to be used
-
-# Then we introduce it all to the ENCODING PROCESS
-steg = embedding(im, binarymessage, squarelength)
-
-# Then we obtain the length in bits of the secret message
-binarymessagelength = len(binarymessage)
-
-# Then we introduce it all to the dembedding PROCESS
-secmes, recons = dembedding(steg, binarymessagelength, squarelength)
-
-# We use the 1D-array secmes, to finally unveil the secret message by turning
-# from bin to string
-unveiled = bintostring(secmes)
-
-# Finally we print the unveiled message and prove that is the same that we
-# introduced early in this example
-print('The unveiled secret message is:', unveiled)
-print('')  # This is to split examples
-print('')
-
-# Plot the most important images
+UNVEILED = ''.join(decod_text)
+print(f'The unveiled secret message is: {UNVEILED} \n')
+# Plot
 plt.figure(1)
 plt.subplot(221)
 plt.imshow(im, cmap='gray')
@@ -230,192 +212,33 @@ plt.imshow(recons, cmap='gray')
 plt.title('Recons Im')
 plt.show()
 
-# %%% 3.2. SECRET IMAGE
-print('EXAMPLE 3.2. SECRET IMAGE')
-
-
-im = data.astronaut()
-
-# Cover image
+print('2. SECRET IMAGE')
+im = data.astronaut()   # Cover image
 im = 0.299 * im[:, :, 0] + 0.587 * \
     im[:, :, 1] + 0.114 * im[:, :, 2]  # Luminance
-
-# Secret image
-secim = rescale(data.camera(), 0.3, anti_aliasing=False)
-
-# Previous process to further unpackbits
-secimflat = np.uint8(secim*255).flatten()
-print('Num of bytes in our message:', len(secimflat))
-
-# We have to obtain our secret image in 1D-array of bits, so we unpackbits
-bitsec = np.unpackbits(secimflat)
-
-squarelength = 8  # square length to be used
-
-# Then we introduce it all to the embedding PROCESS
-steg = embedding(im, bitsec, squarelength)
-
-# Then we obtain the length in bits of the secret message
-binarymessagelength = len(bitsec)
-
-# Then we introduce it all to the dembedding PROCESS
-secmes, recons = dembedding(steg, binarymessagelength, squarelength)
-
-# Once we obtain the 1D binary array we have to pack again to obtain integers
-# (pixel values)
-revealsecflat = np.packbits(secmes)
-# and reshape to obtain back again the secret image
-revealsec = revealsecflat.reshape(np.shape(secim))
-
-db_secim = np.double(secim*255)
-db_revealsec = np.double(revealsec)
-sec_ssim = ssim(db_secim, db_revealsec,
-                data_range=255.0)
-
-print('The SSIM comparing Original Sec Im and Revealed Sec Im is:', sec_ssim)
-print('')
-print('')
-
-# Plot the most important images
-plt.figure(2)
-plt.subplot(231)
-plt.imshow(im, cmap='gray')
-plt.title('Cover (Lum) Im')
-plt.subplot(232)
-plt.imshow(secim, cmap='gray')
-plt.title('Sec Im')
-plt.subplot(233)
-plt.imshow(steg, cmap='gray')
-plt.title('Stego Im')
-plt.subplot(234)
-plt.imshow(recons, cmap='gray')
-plt.title('Reconstructed Im')
-plt.subplot(235)
-plt.imshow(revealsec, cmap='gray')
-plt.title('Reveal Im')
-plt.show()
-
-# %% 4. EXAMPLES WITH HUFFMAN TREE
-
-'''We'll show the implementation of DCT Steganography in two examples:
-character string and secret image which they are compressed by the Huffman
-tree process.'''
-
-
-# %%% 4.1. CHARACTERS STRING
-print('EXAMPLE 4.1. CHARACTERS STRING WITH HUFFMAN TREE')
-
-im = data.camera()  # Cover image
-
-# Secret message, this is the typical one:
-message = 'Steganography among other rare disciplines is honored to be described\
- as both an art and Science field.'
-
-# Firstly we wanna know how many characters has our message
-print('Num of characters in our message:', len(message))
-
-# ______________________________________________________________________________
-# now we use the huffman encoding
-textcod, huffmantree = huffman_encoding(message)
-# ______________________________________________________________________________
-# Secret image in 1D-array of bits, from str textcod
-binarymessage = np.zeros(len(textcod), dtype='uint8')
-
-# Then we obtain the length in bits of the secret message
-binarymessagelength = len(binarymessage)
-# and put the textcod string in the array as uint8
-for i in np.arange(binarymessagelength):
-    binarymessage[i] = np.uint8(textcod[i])
-
-squarelength = 8  # square length to be used
-
-# Then we introduce it all to the embedding PROCESS
-steg = embedding(im, binarymessage, squarelength)
-
-# Then we obtain the length in bits of the secret message
-binarymessagelength = len(binarymessage)
-
-# Then we introduce it all to the dembedding PROCESS
-secmes, recons = dembedding(steg, binarymessagelength, squarelength)
-
-# create a string from secmes 1D-array to decode thanks to huffman tree
-secmes1 = ''.join(str(i) for i in secmes)
-
-# ______________________________________________________________________________
-# decod the huffman code of the image
-textdecod1 = huffman_decoding(secmes1, huffmantree)
-# ______________________________________________________________________________
-
-# We use the list secmes, to finally unveil the secret message by turning
-# from list to string
-unveiled1 = ''.join(i for i in textdecod1)
-
-# Finally we print the unveiled message and prove that is the same that we
-# introduced early in this example
-print('The unveiled secret message is:', unveiled1)
-print('')
-print('')
-
-# Plot the most important images
-plt.figure(3)
-plt.subplot(221)
-plt.imshow(im, cmap='gray')
-plt.title('Cover Im')
-plt.subplot(222)
-plt.imshow(steg, cmap='gray')
-plt.title('Stego Im')
-plt.subplot(223)
-plt.imshow(recons, cmap='gray')
-plt.title('Recons Im')
-plt.show()
-
-# %%% 4.2. SECRET IMAGE
-print('EXAMPLE 4.2. SECRET IMAGE WITH HUFFMAN TREE')
-
-im = data.astronaut()
-
-# Cover image
-im = 0.299 * im[:, :, 0] + 0.587 * \
-    im[:, :, 1] + 0.114 * im[:, :, 2]  # Luminance
-
-# Secret image
-secim = rescale(data.camera(), 0.3, anti_aliasing=False)
+secim = rescale(data.camera(), 0.3, anti_aliasing=False)    # Secret image
 # This is the maximum factor of scale that we can hide 0.34
 # So the next step will be hiding the whole image in a RGB cover image
 
-# ______________________________________________________________________________
-# flatten the image  to pas to a list
-secimflat = np.uint64((secim*255).flatten())
-secimflat = list(secimflat)
-print('Num of bytes in our message:', len(secimflat))
+secimflat = list(np.uint64((secim*255).flatten()))
+print('Num of pixels (bytes) in our message:', len(secimflat))
+encod_text, huffmantree = huffman_encoding(secimflat)
+bitsec = np.array([int(i) for i in encod_text], dtype='uint8')
 
-# now we use the huffman encoding
-textcod, huffmantree = huffman_encoding(secimflat)
-# ______________________________________________________________________________
-# Secret image in 1D-array of bits, from str textcod
-bitsec = np.zeros(len(textcod), dtype='uint8')
+SQR_SIDE = 8    # Size of the blocks to be processed: Common info
+BINMESLEN = len(bitsec)     # Common info
 
-# Then we obtain the length in bits of the secret message
-binarymessagelength = len(bitsec)
-# and put the textcod str in the array as uint8
-for i in np.arange(binarymessagelength):
-    bitsec[i] = np.uint8(textcod[i])
-
-
-squarelength = 8  # square length to be used
-
-# Then we introduce it all to the embedding PROCESS
-steg = embedding(im, bitsec, squarelength)
+steg = embedding(im, bitsec, SQR_SIDE)
 
 # Then we introduce it all to the dembedding PROCESS
-secmes, recons = dembedding(steg, binarymessagelength, squarelength)
+SECMES, recons = dembedding(steg, BINMESLEN, SQR_SIDE)
 
 # create a string from secmes 1D-array to decode thanks to huffman tree
-secmes1 = ''.join(str(i) for i in secmes)
+SECMES = ''.join(str(i) for i in SECMES)
 
 # ______________________________________________________________________________
 # decod the huffman code of the image
-textdecod = huffman_decoding(secmes1, huffmantree)
+textdecod = huffman_decoding(SECMES, huffmantree)
 # ______________________________________________________________________________
 # from textdecod to a 1D-array
 revealsecflat = np.array(textdecod)
@@ -428,11 +251,8 @@ db_revealsec = np.double(revealsec)
 sec_ssim = ssim(db_secim, db_revealsec,
                 data_range=255.0)
 
-print('The SSIM comparing Original Sec Im and Revealed Sec Im is:', sec_ssim)
-print('')
-print('')
-
-# Plot the most important images
+print(f'SSIM Original Sec Im and Revealed Sec Im is: {sec_ssim} \n')
+# Plot
 plt.figure(4)
 plt.subplot(231)
 plt.imshow(im, cmap='gray')
@@ -451,38 +271,23 @@ plt.imshow(revealsec, cmap='gray')
 plt.title('Reveal Im')
 plt.show()
 
-# %%% 4.3. SECRET IMAGE WITH RGB COVER
-print('EXAMPLE 4.3. SECRET IMAGE WITH HUFFMAN TREE and RGB COVER')
 
+print('3. SECRET IMAGE WITH RGB COVER')
 im = data.astronaut()
 imR = im[:, :, 0]
 imG = im[:, :, 1]
 imB = im[:, :, 2]
+secim = rescale(data.camera(), 0.5, anti_aliasing=False)    # Secret image
 
-# Secret image
-secim = rescale(data.camera(), 0.5, anti_aliasing=False)
-# This is the maximum factor of scale that we can hide 0.34
-# So the next step will be hiding the whole image in a RGB cover image
+secimflat = list(np.uint64((secim*255).flatten()))
+print('Num of pixels (bytes) in our message:', len(secimflat))
+encod_text, huffmantree = huffman_encoding(secimflat)
+bitsec = np.array([int(i) for i in encod_text], dtype='uint8')
 
-# ______________________________________________________________________________
-# flatten the image  to pass to a list
-secimflat = np.uint64((secim*255).flatten())
-secimflat = list(secimflat)
-print('Num of bytes in our message:', len(secimflat))
+SQR_SIDE = 8    # Size of the blocks to be processed: Common info
+BINMESLEN = len(bitsec)     # Common info
 
-# now we use the huffman encoding
-textcod, huffmantree = huffman_encoding(secimflat)
-# ______________________________________________________________________________
-# Secret image in 1D-array of bits, from str textcod
-bitsec = np.zeros(len(textcod), dtype='uint8')
-
-# Then we obtain the length in bits of the secret message
-binarymessagelength = len(bitsec)
-# and put the textcod str in the array as uint8
-for i in np.arange(binarymessagelength):
-    bitsec[i] = np.uint8(textcod[i])
-
-div = int(binarymessagelength/3)
+div = BINMESLEN // 3
 bitsecR = bitsec[:div]
 bitsecG = bitsec[div:(div*2)]
 bitsecB = bitsec[(div*2):]
@@ -491,41 +296,39 @@ binmeslenR = len(bitsecR)
 binmeslenG = len(bitsecG)
 binmeslenB = len(bitsecB)
 
-squarelength = 8  # square length to be used
-
 
 # R
 # Then we introduce it all to the embedding PROCESS
-stegR = embedding(imR, bitsecR, squarelength)
+stegR = embedding(imR, bitsecR, SQR_SIDE)
 
 # Then we introduce it all to the dembedding PROCESS
-secmes, reconsR = dembedding(stegR, binmeslenR, squarelength)
+SECMES, reconsR = dembedding(stegR, binmeslenR, SQR_SIDE)
 # create a string from secmes 1D-array to decode thanks to huffman tree
-secmesR = ''.join(str(i) for i in secmes)
+secmesR = ''.join(str(i) for i in SECMES)
 
 # G
 # Then we introduce it all to the embedding PROCESS
-stegG = embedding(imG, bitsecG, squarelength)
+stegG = embedding(imG, bitsecG, SQR_SIDE)
 
 # Then we introduce it all to the dembedding PROCESS
-secmes, reconsG = dembedding(stegG, binmeslenG, squarelength)
+SECMES, reconsG = dembedding(stegG, binmeslenG, SQR_SIDE)
 # create a string from secmes 1D-array to decode thanks to huffman tree
-secmesG = ''.join(str(i) for i in secmes)
+secmesG = ''.join(str(i) for i in SECMES)
 
 # B
 # Then we introduce it all to the embedding PROCESS
-stegB = embedding(imB, bitsecB, squarelength)
+stegB = embedding(imB, bitsecB, SQR_SIDE)
 
 # Then we introduce it all to the dembedding PROCESS
-secmes, reconsB = dembedding(stegB, binmeslenB, squarelength)
+SECMES, reconsB = dembedding(stegB, binmeslenB, SQR_SIDE)
 # create a string from secmes 1D-array to decode thanks to huffman tree
-secmesB = ''.join(str(i) for i in secmes)
+secmesB = ''.join(str(i) for i in SECMES)
 
-secmes = secmesR+secmesG+secmesB  # Finally join the whole secret message str
+SECMES = secmesR+secmesG+secmesB  # Finally join the whole secret message str
 
 # ______________________________________________________________________________
 # decod the huffman code of the image
-textdecod = huffman_decoding(secmes, huffmantree)
+textdecod = huffman_decoding(SECMES, huffmantree)
 # ______________________________________________________________________________
 revealsecflat = np.array(textdecod)  # put in a 1D-array
 
